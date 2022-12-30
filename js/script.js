d82e13d1c8dac04fcd0d48be15928748066105d3
Pa <56890167+p4zaa@users.noreply.github.com>
2022-12-31 00:18:56 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Add a smooth scroll effect when clicking on a nav link
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    document.query
