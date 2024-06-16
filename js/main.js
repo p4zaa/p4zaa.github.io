5f77f03740a82e71d6ca31fca2c57b3dd46532d2
Pa <56890167+p4zaa@users.noreply.github.com>
2024-06-16 23:24:25 +0700

    Add palette grid

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ document.querySelectorAll('.color-palette').forEach(palette => {
     palette.style.setProperty('--palette-color', color);
 
     palette.addEventListener('click', () => {
-        const colorCode = palette.querySelector('.color-code').textContent;
-        navigator.clipboard.writeText(colorCode).then(() => {
-            //alert(`Copied the color code: ${colorCode}`);
+        //const colorCode = palette.querySelector('.color-code').textContent;
+        navigator.clipboard.writeText(color).then(() => {
+            //alert(`Copied the color code: ${color}`);
         }).catch(err => {
             console.error('Failed to copy the text: ', err);
         });
